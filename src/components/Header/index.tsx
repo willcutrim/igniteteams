bbe1048c5b509845341f4b6a00fb4b7b511896a9
willcutrim <willyam894@gmail.com>
2022-11-08 15:54:22 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Container, Logo } from "./styles";
+import logoImg from '@assets/logo.png';
+
+
+
+
+export function Header(){
+    return (
+        <Container>
+            <Logo source={logoImg}/>
+        </Container>
+    );
+
+}
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+
+import { Header } from '@components/Header';
+import { SafeAreaView } from 'react-native';
+import { Container } from './styles';
+
+export function Groups() {
+  return (
+    <Container>
+        <Header/>
+    </Container>
+  );
+}
